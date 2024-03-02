f022aeb0309f971d5439f11e6ad2ccbfda2f1f6b
Harith Onigemo <onigemotosin@gmail.com>
2024-03-02 16:24:34 +0400

    :rocket: Fixing deployment issue

--- TrashClassifier.py
+++ TrashClassifier.py
@@ -1,7 +1,7 @@
-import cv2
+import os
 import yolov5
 
-MODEL_REF= './ai-model/yolov5s.pt'
+MODEL_REF= os.path.join(os.getcwd(), "/ai-model/yolov5s.pt")
 
 class TrashClassifier:
     def __init__(self) -> None:
