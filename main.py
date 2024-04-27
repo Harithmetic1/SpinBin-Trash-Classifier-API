0a3a6258c70a69d5abefbf4196394810c80ee2fc
Harith Onigemo <onigemotosin@gmail.com>
2024-04-27 14:01:17 +0400

    Added rollback route to yOLOv5 model

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from TrashClassifier import TrashClassifier, TrashClassifierV8
 
 app = FastAPI()
 
-# classifierHF = TrashClassifier()
+classifierHF = TrashClassifier()
 classifier = TrashClassifierV8()
 
 @app.get("/")
@@ -56,6 +56,32 @@ async def classify_waste_b64(img: UploadImageSchema):
 
          class_img = Image.open(file_path)
          classification = classifier.classify(class_img)
+         if classification == "metal":
+            return { "classification": "metal"}
+         elif classification == "paper":
+            return {"classification": "paper"}
+         elif classification == "plastic":
+            return {"classification": "plastic"}
+         elif classification == "glass":
+            return {"classification": "glass"}
+         else:
+            return {"classification": "unknown"}
+    except Exception as e:
+        return {"error": str(e)}
+    
+@app.post("/classifybase64HF", response_model=dict)
+async def classify_waste_b64(img: UploadImageSchema):
+    try:
+         # Decode the base64 string into binary data
+         img_data = base64.b64decode(img.img)
+
+         # Create a new file called "pic" in the current directory
+         file_path = os.path.join(os.getcwd(), "pic.png")
+         with open(file_path, "wb") as f:
+            f.write(img_data)
+
+         class_img = Image.open(file_path)
+         classification = classifierHF.classify(class_img)
          if classification == "metal":
             return { "classification": "metal"}
          elif classification == "paper":
